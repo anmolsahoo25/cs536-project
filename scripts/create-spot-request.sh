ae2a8b874792ca6bb2f857e8751581079de4cff2
Anmol Sahoo <anmol.sahoo25@gmail.com>
2022-11-29 19:22:57 -0500

    updated script version

--- scripts/create-spot-request.sh
+++ scripts/create-spot-request.sh
@@ -1,4 +1,4 @@
 aws ec2 run-instances \
-    --launch-template LaunchTemplateName=m5zn_metal_default,Version=2 \
+    --launch-template LaunchTemplateName=m5zn_metal_default,Version=10 \
     --count 1 \
     --instance-market-options file://spot-options.json
