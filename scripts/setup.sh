811794121a3372f66c232e7bfe4beb270652055a
Anmol Sahoo <anmol.sahoo25@gmail.com>
2022-12-05 13:47:57 -0500

    updates

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,12 @@
 cp ./tmux.conf ~/.tmux.conf
+
 sudo yum groupinstall -y 'Development Tools'
-sudo yum install -y perf.x86_64 git
+
+sudo yum install -y perf.x86_64 git tmux docker
+
 sudo sh -c 'echo -1 >/proc/sys/kernel/perf_event_paranoid'
+
+sudo systemctl start docker
+
+git clone --branch kernel-5.10.147-133.644.amzn2 \
+    --single-branch https://github.com/amazonlinux/linux
